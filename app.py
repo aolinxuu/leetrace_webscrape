8b6411cc044031268de63f9f21df27231c932981
Aolin Xu <aolinzamxu@gmail.com>
2025-01-20 00:10:56 +1100

    bind to port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import os
 
 app = Flask(__name__, template_folder='.')
 df = pd.read_csv('clean_res.csv')
@@ -20,5 +21,6 @@ def index():
     return render_template('index.html', topics=topics, companies=companies, problems=filtered_data.to_html())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
